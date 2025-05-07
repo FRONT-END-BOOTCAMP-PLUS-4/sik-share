d76ed522b84b2a05681a120f729a0d64d22fc00f
hoon95 <maruon95@gmail.com>
2025-05-07 09:53:59 +0900

    chore: migrate prisma

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
 	return (
 		<>
+			<h1>H1 h1 제목을 입력하세요</h1>
+			<h2>H2 h2 제목을 입력하세요</h2>
+			<h3>H3 h3 제목을 입력하세요</h3>
 			<Button>Click me!</Button>
 		</>
 	);
