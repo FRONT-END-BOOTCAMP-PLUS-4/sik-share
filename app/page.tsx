054bb1d04c9bfb69adaa83e4bf7ad5592eb48459
hoon95 <maruon95@gmail.com>
2025-05-10 08:24:31 +0900

    style: root style 정의

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,16 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
 	return (
 		<>
-			<h1>H1 h1 제목을 입력하세요</h1>
-			<h2>H2 h2 제목을 입력하세요</h2>
-			<h3>H3 h3 제목을 입력하세요</h3>
+			<p className="title-lg">lg 제목을 입력하세요</p>
+			<p className="title-md">md 제목을 입력하세요</p>
+			<p className="title-sm">sm 제목을 입력하세요</p>
+
+			<br />
+
+			<p className="body-lg">lg 본문 내용입니다.</p>
+			<p className="body-md">md 본문 내용입니다.</p>
+			<p className="body-sm">sm 본문 내용입니다.</p>
+
 			<Button>Click me!</Button>
 		</>
 	);
