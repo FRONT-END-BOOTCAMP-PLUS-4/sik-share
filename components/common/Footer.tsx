65bcbf9f32746c9c83e43b08937078d0d496781d
hoon95 <maruon95@gmail.com>
2025-05-10 15:47:39 +0900

    style: Footer - pathname에 따른 색상 변경

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
 import {
 	House,
 	Salad,
@@ -7,24 +10,43 @@ import {
 } from "lucide-react";
 
 const navItems = [
-	{ Icon: House, label: "홈" },
-	{ Icon: Salad, label: "나눔함" },
-	{ Icon: MapPinned, label: "동네지도" },
-	{ Icon: MessageCircle, label: "채팅" },
-	{ Icon: CircleUserRound, label: "나의 식샤" },
+	{ key: "", Icon: House, label: "홈" },
+	{ key: "share", Icon: Salad, label: "나눔함" },
+	{ key: "map", Icon: MapPinned, label: "동네지도" },
+	{ key: "chat", Icon: MessageCircle, label: "채팅" },
+	{ key: "profile", Icon: CircleUserRound, label: "나의 식샤" },
 ] as const;
 
 export default function Footer() {
+	const pathname = usePathname();
+	const router = useRouter();
+
+	const current = pathname?.slice(1) ?? navItems[0].key;
+	const activeTab =
+		navItems.find((item) => item.key === current)?.key ?? navItems[0].key;
+
+	const handleClick = (key: string) => {
+		router.push(`/${key}`);
+	};
+
 	return (
-		<footer className="z-10 fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[var(--space-mobileMax)] flex justify-between items-center bg-white min-h-[var(--space-header)] px-4 py-2">
-			<ul className="w-full flex justify-around items-center gap-[var(--space-footer)]">
-				{navItems.map(({ Icon, label }) => (
-					<li key={label} className="flex flex-col items-center cursor-pointer">
-						<Icon />
-						<p className="label-11">{label}</p>
-					</li>
-				))}
-			</ul>
+		<footer className="z-10 fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[var(--space-mobileMax)] bg-white flex justify-around items-center min-h-[var(--space-header)] px-4 py-2">
+			{navItems.map(({ key, Icon, label }) => {
+				const isActive = key === activeTab;
+				const colorClass = isActive ? "text-primary" : "";
+
+				return (
+					// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+					<div
+						key={key}
+						className="flex flex-col items-center cursor-pointer"
+						onClick={() => handleClick(key)}
+					>
+						<Icon className={`${colorClass} w-6 h-6`} />
+						<span className={`${colorClass} label`}>{label}</span>
+					</div>
+				);
+			})}
 		</footer>
 	);
 }
