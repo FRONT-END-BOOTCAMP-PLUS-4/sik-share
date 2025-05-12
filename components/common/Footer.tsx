4c22fb600f53d429ceca38d3026fa57425fe53d4
hoon95 <maruon95@gmail.com>
2025-05-12 21:14:01 +0900

    style: Footer 반응형 설정(lg 구간)

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,51 +2,59 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import {
-	House,
-	Salad,
-	MapPinned,
-	MessageCircle,
-	CircleUserRound,
+  House,
+  Salad,
+  MapPinned,
+  MessageCircle,
+  CircleUserRound,
 } from "lucide-react";
 
 const navItems = [
-	{ key: "", Icon: House, label: "홈" },
-	{ key: "share", Icon: Salad, label: "나눔함" },
-	{ key: "map", Icon: MapPinned, label: "동네지도" },
-	{ key: "chat", Icon: MessageCircle, label: "채팅" },
-	{ key: "profile", Icon: CircleUserRound, label: "나의 식샤" },
+  { key: "", Icon: House, label: "홈" },
+  { key: "share", Icon: Salad, label: "나눔함" },
+  { key: "map", Icon: MapPinned, label: "동네지도" },
+  { key: "chat", Icon: MessageCircle, label: "채팅" },
+  { key: "profile", Icon: CircleUserRound, label: "나의 식샤" },
 ] as const;
 
 export default function Footer() {
-	const pathname = usePathname();
-	const router = useRouter();
+  const pathname = usePathname();
+  const router = useRouter();
 
-	const current = pathname?.slice(1) ?? navItems[0].key;
-	const activeTab =
-		navItems.find((item) => item.key === current)?.key ?? navItems[0].key;
+  const current = pathname?.slice(1) ?? navItems[0].key;
+  const activeTab =
+    navItems.find((item) => item.key === current)?.key ?? navItems[0].key;
 
-	const handleClick = (key: string) => {
-		router.push(`/${key}`);
-	};
+  const handleClick = (key: string) => {
+    router.push(`/${key}`);
+  };
 
-	return (
-		<footer className="z-10 fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[var(--space-mobileMax)] bg-white flex justify-around items-center min-h-[var(--space-header)] px-4 py-2 shadow-[var(--bottom-nav-shadow)] border-x border-zinc-300">
-			{navItems.map(({ key, Icon, label }) => {
-				const isActive = key === activeTab;
-				const colorClass = isActive ? "text-primary" : "";
+  return (
+    <footer
+      className="z-10
+        fixed inset-x-0 bottom-0
+        mx-auto max-w-[var(--space-mobileMax)]
+        bg-white flex justify-around items-center
+        min-h-[var(--space-header)] px-4 py-2
+        shadow-[var(--bottom-nav-shadow)] border-x border-zinc-300
+        lg:absolute lg:inset-x-0 lg:bottom-0"
+    >
+      {navItems.map(({ key, Icon, label }) => {
+        const isActive = key === activeTab;
+        const colorClass = isActive ? "text-primary" : "";
 
-				return (
-					// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-					<div
-						key={key}
-						className="flex flex-col items-center cursor-pointer"
-						onClick={() => handleClick(key)}
-					>
-						<Icon className={`${colorClass}`} />
-						<span className={`${colorClass} label`}>{label}</span>
-					</div>
-				);
-			})}
-		</footer>
-	);
+        return (
+          // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+          <div
+            key={key}
+            className="flex flex-col items-center cursor-pointer"
+            onClick={() => handleClick(key)}
+          >
+            <Icon className={`${colorClass}`} />
+            <span className={`${colorClass} label`}>{label}</span>
+          </div>
+        );
+      })}
+    </footer>
+  );
 }
