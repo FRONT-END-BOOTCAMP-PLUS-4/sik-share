427fde04447703f0edf05a8f2488385363889522
hoon95 <maruon95@gmail.com>
2025-05-14 15:15:29 +0900

    style: 모바일 구간 Food lottie 크기 수정

--- components/lotties/FoodLottie.tsx
+++ components/lotties/FoodLottie.tsx
@@ -4,7 +4,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function FoodLottie() {
   return (
-    <div className="w-[200px] h-[100px] sm:w-auto sm:h-auto">
+    <div className="w-[300px] h-[150px] sm:w-auto sm:h-auto">
       <DotLottieReact src="/assets/lotties/Food.lottie" loop autoplay />
     </div>
   );
