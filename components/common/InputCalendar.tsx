df9c629a1676c0b660e489e53196ae45b3f0cf74
Eunji <eunjipark0129@gmail.com>
2025-05-23 10:11:22 +0900

    fix : InputCalendar 위치 start(왼쪽) > end(오른쪽)로 변경

--- components/common/InputCalendar.tsx
+++ components/common/InputCalendar.tsx
@@ -42,7 +42,7 @@ export default function InputCalendar({
         </Button>
       </PopoverTrigger>
 
-      <PopoverContent className="w-auto p-0" align="start">
+      <PopoverContent className="w-auto p-0" align="end">
         <Calendar selected={value} onSelect={onChange} />
       </PopoverContent>
     </Popover>
