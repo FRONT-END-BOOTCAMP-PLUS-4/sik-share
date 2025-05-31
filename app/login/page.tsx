396bb52044fa50d40298cd0951cfb5a487d8c120
hoon95 <maruon95@gmail.com>
2025-06-01 00:11:08 +0900

    style: 로그인 페이지 2안 적용

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,17 +3,23 @@
 import SubHeader from "@/components/common/SubHeader";
 import ButtonSection from "./components/ButtonSection";
 import Title from "./components/Title";
+import Image from "next/image";
 
 export default function AuthPage() {
   return (
     <>
       <SubHeader />
-      <section className="px-11 h-[calc(100vh-171px)]">
-        <div className="h-full flex flex-col justify-around">
+      <section className="relative h-[calc(100vh-171px)]">
+        <div className="px-11 h-full flex flex-col gap-[60px] mt-[60px]">
           <Title />
-          <div className="flex justify-center">로티</div>
           <ButtonSection />
         </div>
+        <Image
+          src="/assets/images/login/image.png"
+          alt="로그인 페이지 하단 홍보용 이미지"
+          fill
+          className="!top-36 object-cover pointer-events-none"
+        />
       </section>
     </>
   );
