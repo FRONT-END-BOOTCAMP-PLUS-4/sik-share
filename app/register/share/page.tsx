de415a135aafb1cfb056a2cc0d2c1e9cc5deb11b
Eunji <eunjipark0129@gmail.com>
2025-06-01 15:18:41 +0900

    fix: 나눔, 장보기 등록 후 뒤로 가기 시 두 번 이동되는 오류 수정

--- app/register/share/page.tsx
+++ app/register/share/page.tsx
@@ -89,7 +89,6 @@ export default function CreateSharePage() {
           router.back();
         },
       });
-      router.back();
     } catch (error) {
       console.error("나눔 등록 중 오류 발생:", error);
       toast.error("네트워크 오류가 발생했습니다. 다시 시도해주세요.");
