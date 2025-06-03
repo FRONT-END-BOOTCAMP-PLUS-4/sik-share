5546eaef1d66e9b593e0dc672504dc9e79bd08b7
haSong <70552536+poan1221@users.noreply.github.com>
2025-06-03 16:06:49 +0900

    style: 모바일 웹용 높이 조절

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,7 @@ export default function AuthPage() {
   return (
     <>
       <SubHeader />
-      <section className="relative h-[calc(100vh-171px)]">
+      <section className="relative h-[calc(100svh-62px)]">
         <div className="px-11 h-full flex flex-col justify-center gap-[60px]">
           <Title />
           <ButtonSection />
